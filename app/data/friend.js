f5c2e2d1e565c0fc2ebdc83b63dda1ef7ecb7dd7
Vis Vasanth <viscodesdpt@gmail.com>
2016-10-28 23:10:35 -0500

    CssNotWorking, need to create Matchfunction

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,6 +1,6 @@
 var friendsArray = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Bob",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/c/c5/Bob_the_builder.jpg",
     "scores": [
         5,
         1,
@@ -12,6 +12,22 @@ var friendsArray = [{
         5,
         4,
         1
+    ]
+},
+{
+    "name": "Boov",
+    "photo": "http://3.bp.blogspot.com/-jJee3JnH1DU/VQdmgMKfs7I/AAAAAAAAGvg/L7rnE-zatgY/s1600/oh.PNG",
+    "scores": [
+        1,
+        5,
+        2,
+        2,
+        1,
+        5,
+        3,
+        1,
+        2,
+        5
     ]
 }];
 
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -2,20 +2,22 @@
 // DEPENDENCIES
 // ==============================================================================
 var path = require('path');
+var express = require('express');
+var app = express();
 
 // ==============================================================================
 // ROUTING 
 // ==============================================================================
+
+//app.use('/static', express.static('public'));
 module.exports = function(app) {
+    app.use(express.static('./public'));
+//        app.use(express.static(__dirname+'/public'));
     // HTML GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases the user is shown an HTML page of content
     // ---------------------------------------------------------------------------
 
-    // app.get('/', function (req, res) {
-    // 	//res.sendFile(path.join(__dirname + '/../public/home.html'));
-    // 	res.send("root");
-    // });
 
     app.get('/survey', function(req, res) {
     	//res.json("helloworld");
