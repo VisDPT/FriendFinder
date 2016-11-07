865555edd46f855c8a1ead16e1e6773d458eb131
Vis Vasanth <viscodesdpt@gmail.com>
2016-11-07 14:56:50 -0600

    FormSubmission posts to API with match alert, needs match function

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,55 +4,40 @@
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-var friendsList 		= require('../data/friend.js');
+var friendsList = require('../data/friend.js');
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-module.exports = function (app) {
-	// API GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases when a user visits a link
-	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-	// ---------------------------------------------------------------------------
+module.exports = function(app) {
+    // API GET Requests
 
-	app.get('/api/friend', function (req, res) {
-		res.json(friendsList);
-	});
+    app.get('/api/friend', function(req, res) {
+        res.json(friendsList);
+    });
 
-	// API POST Requests
-	// Below code handles when a user submits a form and thus submits data to the server.
-	// In each of the below cases, when a user submits form data (a JSON object)
-	// ...the JSON is pushed to the appropriate Javascript array
-	// (ex. User fills out a reservation request... this data is then sent to the server...
-	// Then the server saves the data to the tableData array)
-	// ---------------------------------------------------------------------------
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate Javascript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
 
-	app.post('/api/friend', function (req, res) {
-		if (friendsList.length < 5) {
-			friendsList.push(req.body);
-			res.json({reservation: true}); // KEY LINE
-		} else { // Or false if they don't have a table
-			friendsList.push(req.body);
-			res.json({reservation: false}); // KEY LINE
-		}
-	});
+    app.post('/api/friend', function(req, res) {
 
-	// ---------------------------------------------------------------------------
-	// I added this below code so you could clear out the table while working with the functionality.
-	// Don't worry about it!
+        friendsList.push(req.body);
+        res.json(true);
+    });
 
-	app.post('/api/clear', function () {
-		// Empty out the arrays of data
-		friendsList = [];
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out form
 
-		console.log(friendsList);
-	});
+    app.post('/api/clear', function() {
+        // Empty out the arrays of data
+        friendsList = [];
+
+        console.log(friendsList);
+    });
 };
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -14,14 +14,8 @@ module.exports = function(app) {
     // app.use('/static', express.static(__dirname + '/public'));
 
 
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
-
-
     app.get('/survey', function(req, res) {
-    	//res.json("helloworld");
+        //res.json("helloworld");
         res.sendFile(path.join(__dirname, '/../public/survey.html'));
         // if (err) throw err;
         // console.log(err);
